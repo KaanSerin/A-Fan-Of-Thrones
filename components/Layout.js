6f303a87949015b10bf24b3e2852439920d1018e
Kaan Serin <kaanserin113@gmail.com>
2020-08-29 14:01:20 +0300

    Changed the favicon. Added some meta tags

--- components/Layout.js
+++ components/Layout.js
@@ -30,6 +30,15 @@ const Layout = ({ children }) => {
       <Head>
         <title>A Fan of Thrones</title>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <meta
+          name="description"
+          content="A fan website for George R. R. Martin's 'A Song of Ice and Fire' book series and HBO's Game of Thrones."
+        />
+        <meta
+          name="keywords"
+          content="A Song of Ice and Fire, A Game of Thrones, George R. R. Martin, Fansite, HBO"
+        />
+        <meta name="author" content="Kaan Serin" />
         <link
           rel="stylesheet"
           type="text/css"
